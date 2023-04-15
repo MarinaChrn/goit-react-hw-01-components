8d8af2eae0780c495b2793ce882ccf9dedd51ada
MarinaChorna <mrnchrn06@gmail.com>
2023-04-15 23:40:09 +0300

    Завдання 1 без стилів

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+import { Profile } from "./profile/Profile";
+import user from './user';
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
-        display: 'flex',
         justifyContent: 'center',
+        marginLeft: '50px',
         alignItems: 'center',
-        fontSize: 40,
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile  username={user.username}
+                tag={user.tag}
+                location={user.location}
+                avatar={user.avatar}
+                stats={user.stats}/>
     </div>
   );
 };
